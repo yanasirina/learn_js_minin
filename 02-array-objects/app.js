c567fe39e5b5be3a10224611efd613f65aa592ab
Yana Sirina <yanasirina0@yandex.ru>
2023-11-20 23:20:49 +0500

    демонстрация методов массива в todo листе

--- 02-array-objects/app.js
+++ 02-array-objects/app.js
@@ -43,11 +43,20 @@ function printNote(note, index) {
 
 }
 
+function sortNotes() {
+    notes.sort(function(a, b) {  // функция - необязательный аргумент
+        return a.title.charCodeAt() - b.title.charCodeAt()  // сортировка по алфавиту (по нулевому символу названия задачи)
+    })
+    // метод sort изменяет массив, метод toSorted возвращает новый массив (аналогично reverse, toReversed)
+    // notes.sort((a, b) => a.title.charCodeAt() - b.title.charCodeAt())  // вариант со стрелочной функцией
+}
+
 function printNotes() {
     listElement.innerHTML = ''  // очистим текущее содержимое блока для дальнейшей перезаписи
     if (notes.length === 0) {
         listElement.innerHTML = '<p>Нет заметок</p>'
     }
+    sortNotes()
     for (let i = 0; i < notes.length; i++) {
         printNote(notes[i], i)
     }
@@ -60,7 +69,9 @@ function printNotes() {
 }
 
 function removeNote(note_index) {
-    notes.splice(start=note_index, deleteCount=1)  // удаление элементов начиная с индекса start в количестве deleteCount
+    deletedTask = notes.splice(start=note_index, deleteCount=1)[0]  // удаление элементов начиная с индекса start в количестве deleteCount (по умолчанию до конца)
+    // метрод splice изменяет массив, метод toSpliced возвращает срез без изменения массива
+    console.log(`Задача ${deletedTask.title} удалена`)
 }
 
 function toggleNote(note_index) {
@@ -75,7 +86,7 @@ createBtn.onclick = function() {
         title: inputElement.value,
         isCompleted: false
     }
-    notes.push(note)
+    notes.push(note)  // последний элемент (pop/push), первый элемент (shift/unshift). shift и pop возвращают удаленный элемент 
     inputElement.value = ''
     printNotes()
 }
